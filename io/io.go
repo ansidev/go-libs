f4069eda93d88846d8e098d901f2e68800061413
Le Minh Tri <ansidev@gmail.com>
2023-01-31 12:28:37 +0700

    feat(pkg-io): add utility functions for io

---
+++ io/io.go
@@ -0,0 +1,34 @@
+package io
+
+import (
+	"os"
+)
+
+func CreateDirIfNotExists(dirPath string) error {
+	_, err := os.Stat(dirPath)
+
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(dirPath, 0755)
+		if errDir != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func WriteFile(filePath string, content []byte) error {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err := f.Write(content); err != nil {
+		return err
+	}
+
+	return nil
+}
